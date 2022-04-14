2d4e0d17752e89aab9332e7978f662de4d3104d2
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-04-14 15:05:18 -0400

    Implementar swagger

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const  express = require('express');
 const  bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+
 const { config } = require('../config');
 const user = require('./components/user/network');
 
 const  app = express();
 
 app.use(bodyParser.json());
-
+const swaggerDoc = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/user', user);
+
 
 app.listen(config.api.port, () => {
   console.log('API escuchando en el puerto ', config.api.port);
